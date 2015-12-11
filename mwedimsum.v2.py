589aae0efada5e95c527eef1d70ebb3ebf4efbbe
Phil Nguyen <pcn@cs.umd.edu>
2015-12-10 20:55:57 -0500

    add functions for loading from wordnet files; try counting how many time noun/verb appears in a MWE

--- mwedimsum.v2.py
+++ mwedimsum.v2.py
@@ -44,6 +44,31 @@ valid_labels = {'n.act': 0,
 }
 valid_labels_rev = { v:k for k,v in valid_labels.iteritems() }
 
+# convert file to set of words, e.g. "make_a_point" -> ("make", "a", "point")
+def file_2_phrases(filename):
+    with open(filename) as f:
+        return {tuple(l.rstrip('\n').split('_')) for l in f}
+
+# convert file to mappings, e.g. make -> {"make_a_point", "make_up", etc.} ...
+def file_2_mappings(filename):
+    mappings = dict()
+    with open(filename) as f:
+        for l in f:
+            l1 = l.rstrip('\n')
+            tokens = tuple(l1.split('_'))
+            for token in tokens:
+                s = mappings.get(token, set())
+                s.add(l1)
+                mappings[token] = s
+    return mappings
+
+mwe_nouns = file_2_phrases('nouns_mwes_in_wordnet3.1.txt')
+mwe_verbs = file_2_phrases('verbs_mwes_in_wordnet3.1.txt') 
+mapping_nouns = file_2_mappings('nouns_mwes_in_wordnet3.1.txt')
+mapping_verbs = file_2_mappings('verbs_mwes_in_wordnet3.1.txt')
+print "done loading %d nouns and %s verbs" % (len(mwe_nouns), len(mwe_verbs))
+print "%d entries for nouns and %d for verbs in mappings" % (len(mapping_nouns), len(mapping_verbs))
+
 class BIO:
     # construct a BIO object using a bio type ('O', 'B' or 'I') and a
     # optionally a label (that can be used to capture the supersense tag). 
@@ -146,6 +171,20 @@ def compute_reference(prev, truth):
             return [truth]
             # if I return BIO('O',label). What will be the label.
 
+# Extract a sentence's first noun and verb
+def find_first(sentence):
+    res = []
+
+    def go(target_pos):
+        for lex in sentence:
+            label,word,lemma,pos = lex
+            if pos == target_pos:
+                res.append(lex)
+                return
+  
+    go("NOUN")
+    go("VERB")
+    return res
         
 class MWE(pyvw.SearchTask):
     def __init__(self, vw, sch, num_actions):
@@ -164,7 +203,14 @@ class MWE(pyvw.SearchTask):
             # label is a BIO, word is a string and pos is a string
             label,word,lemma,pos = sentence[n]
 
-            with self.make_example(word, lemma, pos) as ex:  # construct the VW example
+            # see how many time the noun/verb appears in a MWE
+            extra = {
+                "NOUN": len(mapping_nouns.get(word, ())),
+                "VERB": len(mapping_verbs.get(word, ()))
+            }.get(pos, 0)
+            print extra
+
+            with self.make_example(word, lemma, pos, extra) as ex:  # construct the VW example
                 # first, compute the numeric labels for all valid reference actions
                 refs  = [ bio.numeric_label for bio in compute_reference(prev, label) ]
                 #print "refs"
@@ -195,11 +241,12 @@ class MWE(pyvw.SearchTask):
         # return the list of predictions as BIO labels
         return output
 
-    def make_example(self, word, lemma, pos):
+    def make_example(self, word, lemma, pos, extra):
         return self.example({
             'w': [word],
             'l': [lemma],
             'p': [pos],
+            'e': [extra]
         })
 
 
